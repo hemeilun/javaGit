6b554a92a4f3337fd42aa5179ff1bc56065c98b5
hemeilun <1580429724@qq.com>
2021-09-27 22:21:53 +0800

    this is hot-fix first

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.printf( "nihao");
         System.out.printf( "nihao1");
+        System.out.println("nihao2");
     }
 }
