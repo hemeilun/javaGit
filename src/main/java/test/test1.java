fd3dc024f424dc76f5e534b7ecaa49591539c0d5
hemeilun <1580429724@qq.com>
2021-09-27 22:35:08 +0800

    this is hot-fix third

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -6,6 +6,7 @@ public class test1 {
         System.out.printf( "nihao1");
         System.out.println("nihao2");
         System.out.println("nihao3");
+        System.out.println("hot-fix");
 
 
     }
