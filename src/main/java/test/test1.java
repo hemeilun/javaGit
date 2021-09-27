4a7d8096c7986e4b80dbe513942066ca752133b5
hemeilun <1580429724@qq.com>
2021-09-27 22:36:16 +0800

    this is master third

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -5,5 +5,7 @@ public class test1 {
         System.out.printf( "nihao");
         System.out.printf( "nihao1");
         System.out.println("nihao2");
+        System.out.println("nihao3");
+        System.out.println("master");
     }
 }
